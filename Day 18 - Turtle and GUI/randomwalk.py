e815ef61cf032f8c564ce19013782cd71acf4e37
Raghavendra <raghavendra8076@gmail.com>
2023-09-25 12:52:03 +0530

    V2.0.2

--- Day 18 - Turtle and GUI/randomwalk.py
+++ Day 18 - Turtle and GUI/randomwalk.py
@@ -7,8 +7,15 @@ colours = ['CornFlowerBlue', 'DarkOrchid', 'IndianRed', 'DeepSkyBlue', 'LightSea
 
 directions = [0, 90, 180, 270]
 
-for _ in range(20):
+tim.pensize(7)
+
+tim.speed("fastest")
+
+for _ in range(100):
     tim.color(random.choice(colours))
     tim.forward(30)
     tim.setheading(random.choice(directions))
+
+screen = t.Screen()
+screen.exitonclick()
 
