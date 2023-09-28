39ff5f088e7c4166ca299ae9964c71c85257b58a
Raghavendra <raghavendra8076@gmail.com>
2023-09-28 16:40:59 +0530

    V1.0

--- Day 20 and Day 21 - Snake Game/snake.py
+++ Day 20 and Day 21 - Snake Game/snake.py
@@ -2,10 +2,15 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0,0),(-20,0),(-40,0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
 
     def create_snake(self):
@@ -21,7 +26,23 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
 #
 # starting_positions = [(0,0),(-20,0),(-40,0)]
