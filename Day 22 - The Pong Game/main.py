a51037aecfb8c69e53ba8bfd30e4342ac18f9f4f
Raghavendra <raghavendra8076@gmail.com>
2023-10-03 13:10:58 +0530

    V1

---
+++ Day 22 - The Pong Game/main.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+screen.setup(height=600, width=800)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
+
+
+
+
+
+
+
+
+screen.exitonclick()
