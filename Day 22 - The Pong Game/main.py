ec98cf1d77d2436a6bd1bbf5386b85929e70a02f
Raghavendra <raghavendra8076@gmail.com>
2023-10-04 11:34:20 +0530

    V1

--- Day 22 - The Pong Game/main.py
+++ Day 22 - The Pong Game/main.py
@@ -1,5 +1,8 @@
 from turtle import Turtle, Screen
-
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.setup(height=600, width=800)
@@ -8,9 +11,39 @@ screen.title("Pong Game")
 screen.tracer(0)
 
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 
 
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
+is_game_on = True
+while is_game_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    # Detecting collision with the top and bottom wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() > -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.ball_reset()
+        scoreboard.l_point()
+
+
+    if ball.xcor() < -380:
+        ball.ball_reset()
+        scoreboard.r_point()
 
 
 
