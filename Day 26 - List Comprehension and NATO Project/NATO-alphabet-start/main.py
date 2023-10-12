cd2221f106ceecd783ae1572305a2245b21cefa7
Raghavendra <raghavendra8076@gmail.com>
2023-10-12 10:47:05 +0530

    V1

--- Day 26 - List Comprehension and NATO Project/NATO-alphabet-start/main.py
+++ Day 26 - List Comprehension and NATO Project/NATO-alphabet-start/main.py
@@ -3,11 +3,11 @@ import pandas
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-# TODO 1. Create a dictionary in this format:
+"""TODO 1. Create a dictionary in this format:"""
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+"""TODO 2. Create a list of the phonetic code words from a word that the user inputs."""
 word = input("Enter a word: ").upper()
 word_list = [phonetic_dict[letter] for letter in word]
 print(word_list)
